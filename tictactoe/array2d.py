785ef3bffb2e4febf411d1696ea26b5daa1f2a87
camlost <jiri.novak@gmail.com>
2021-02-21 07:57:24 +0100

    Add array2d module

---
+++ tictactoe/array2d.py
@@ -0,0 +1,94 @@
+"""
+Array2D module
+"""
+
+from typing import Any,List
+
+
+class CoordinateError(Exception):
+    """Exception raised in case of wrong coordinates.
+    """
+    pass
+
+
+class Array2D:
+    """Array2D class is a base class for Board class.
+    """
+
+    def __init__(self, size_x: int, size_y: int, empty_field: Any):
+        """Class constructor.
+
+        Initialize 2D array with empty values, set sizes.
+
+        Args:
+            size_x (int): Width of array (number of columns)
+            size_y (int): Height of array (number of rows)
+            empty_field (Any): Initial / empty value
+        """
+
+        self.size_x = size_x
+        self.size_y = size_y
+        self._fields = []
+        self._empty_field = empty_field
+        self.clear()
+
+
+    def clear(self):
+        """Clear array.
+
+        Set all fields to empty value.
+        """
+
+        self._fields = []
+        for _ in range(self.size_y):
+            row = [self._empty_field] * self.size_x
+            self._fields.append(row)
+
+
+    def get_vector(self, x: int, y: int, dx: int, dy: int) -> List[Any]:
+        """Get vector.
+
+        Args:
+            x (int): Column of starting field
+            y (int): Row of starting field
+            dx (int): Delta X (usually -1, 0 or 1) used to iterate over fields
+            dy (int): Delta Y (usually -1, 0, or 1) used to iterate over fields
+
+        Returns:
+            List[Any]: List of fields in the vector
+        """
+
+        if x not in range(self.size_x) or y not in range(self.size_y):
+            raise CoordinateError(f"Invalid coordinates [{x}, {y}]")
+        result = []
+        i, j = (x, y)
+        while i < self.size_x and j < self.size_y:
+            result.append(self._fields[i, j])
+            i, j = (i + dx, j + dy)
+        return result
+
+
+    def get_column(self, x: int) -> List[Any]:
+        """Get fields from column.
+
+        Args:
+            x (int): Column of starting field
+
+        Returns:
+            List[Any]: List of fields in the column
+        """
+
+        return self.get_vector(x, 0, 0, 1)
+
+
+    def get_row(self, y: int) -> List[Any]:
+        """Get fields from row.
+
+        Args:
+            y (int): Row of starting field
+
+        Returns:
+            List[Any]: List of fields in the row
+        """
+
+        return self.get_vector(0, y, 1, 0)
